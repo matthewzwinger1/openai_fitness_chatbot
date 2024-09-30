41870e6b0bd163758198645e498a2d8c9a382a75
MatthewZwinger1 <matthewzwinger@gmail.com>
2024-09-30 09:16:31 -0500

    demo

---
+++ app.py
@@ -0,0 +1,29 @@
+from openai import AsyncOpenAI
+import chainlit as cl
+client = AsyncOpenAI()
+
+# Instrument the OpenAI client
+cl.instrument_openai()
+
+settings = {
+    "model": "gpt-4o-mini",
+    "temperature": 0,
+    # ... more settings
+}
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    response = await client.chat.completions.create(
+        messages=[
+            {
+                "content": "You are a helpful fitness trainer with the goal of assisting people in their health journey.",
+                "role": "system"
+            },
+            {
+                "content": message.content,
+                "role": "user"
+            }
+        ],
+        **settings
+    )
+    await cl.Message(content=response.choices[0].message.content).send()
